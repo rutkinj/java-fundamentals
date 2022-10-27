3e38f1edde5c9d14202998c0943b863a1f47a712
Joe Rutkin <rutkinj@gmail.com>
2022-10-26 18:59:23 -0700

    adds maps and file i/o

---
+++ linter/app/src/test/resources/fiveErr.js
@@ -0,0 +1,16 @@
+function and(a,b) {
+  if (a === 1 && b === 1) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+function and(a,b) {
+  if (a === 1 && b === 1) {
+    return 1
+  } else {
+    console.log("bingbong dootdoot")
+    return 0
+  }
+}
